2a057eea9b83dcf708346ab983fdf8e095e27528
Stanley Fujimoto <sfujimoto@gmail.com>
2019-11-22 15:40:01 -0700

    add warnings, remove debug

--- maskrcnn_benchmark/modeling/roi_heads/keypoint_head/roi_keypoint_predictors_objectness.py
+++ maskrcnn_benchmark/modeling/roi_heads/keypoint_head/roi_keypoint_predictors_objectness.py
@@ -15,7 +15,6 @@ class KeypointObjectnessPredictor(nn.Module):
         self.conv1_kps_obj = layers.Conv2d(input_features, input_features * 2, 5, 2)
         self.pool1_kps_obj = nn.AdaptiveAvgPool2d((1, 1))
         self.fc_kps_obj = nn.Linear(input_features * 2, self.num_keypoints * 2)
-        #self.fc_kps_coords = nn.Linear(input_features * 2, self.num_keypoints * 8)
 
         nn.init.kaiming_normal_(self.conv1_kps_obj.weight, mode='fan_out', nonlinearity='relu')
         nn.init.constant_(self.conv1_kps_obj.bias, 0)
@@ -25,14 +24,8 @@ class KeypointObjectnessPredictor(nn.Module):
         x = self.conv1_kps_obj(x)
         x = F.relu(x)
         x = self.pool1_kps_obj(x)
-        #print('x shape', x.shape)
         x = torch.flatten(x, 1)
-        #print('x shape', x.shape)
         obj = self.fc_kps_obj(x).view(N, self.num_keypoints, 2)
-        #print('obj shape', obj.shape)
-        #coords = self.fc_kps_coords(x).view(N, self.num_keypoints, 8)
-        #coords = F.relu(coords)
-        #return obj, coords
         return obj
 
 def make_keypoint_objectness_predictor(cfg, in_channels):
